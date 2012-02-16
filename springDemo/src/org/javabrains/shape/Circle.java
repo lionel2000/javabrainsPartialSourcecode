1513607dc266118e4e7630e922090d3b5908d841
lionel <8836272@qq.com>
2012-02-17 06:02:50 +0800

    don't forget to change the xml file when add @required

---
+++ springDemo/src/org/javabrains/shape/Circle.java
@@ -0,0 +1,22 @@
+package org.javabrains.shape;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Required;
+
+public class Circle {
+	public static Logger log = Logger.getLogger(Circle.class);
+	private Point center;
+
+	public Point getCenter() {
+		return center;
+	}
+    @Required
+	public void setCenter(Point center) {
+		this.center = center;
+	}
+	
+	public void draw(){
+		log.info("Circle draw method is called and the center is " + center.getX());
+	}
+
+}
--- springDemo/src/org/javabrains/study/DrawingApp.java
+++ springDemo/src/org/javabrains/study/DrawingApp.java
@@ -1,13 +1,9 @@
 package org.javabrains.study;
 
 import org.apache.log4j.Logger;
-import org.javabrains.shape.Square;
-import org.javabrains.shape.Triangle;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.javabrains.shape.Circle;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.FileSystemResource;
 
 
 public class DrawingApp {
@@ -25,8 +21,8 @@ public class DrawingApp {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 //		Triangle triangle = (Triangle) context.getBean("triangle");
 //		triangle.draw();
-		Square sq = (Square) context.getBean("square");
-		sq.draw();
+		Circle cl = (Circle) context.getBean("circle");
+		cl.draw();
         
 	}
 
