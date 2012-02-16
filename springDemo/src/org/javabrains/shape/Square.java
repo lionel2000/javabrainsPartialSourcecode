32668e36a032355f4fc28f0152f5ffb28e237ce5
lionel <8836272@qq.com>
2012-02-16 16:31:59 +0800

    BeanNameAware and ApplicationContextAware interface there must be many Aware interface to be used!!!!

--- springDemo/src/org/javabrains/shape/Square.java
+++ springDemo/src/org/javabrains/shape/Square.java
@@ -1,10 +1,17 @@
 package org.javabrains.shape;
 
-public class Square {
+import org.apache.log4j.Logger;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Square implements ApplicationContextAware,BeanNameAware {
+	public static Logger log = Logger.getLogger(Square.class);
 	private Point pointA;
 	private Point pointB;
 	private Point pointC;
-	
+	private ApplicationContext context;
 	public Point getPointA() {
 		return pointA;
 	}
@@ -31,5 +38,17 @@ public class Square {
 
 	public void draw(){
 		System.out.println("x:" + pointA +"y:" + pointB +"z:" + pointC );
+	}
+
+	@Override
+	public void setBeanName(String beanName) {
+	     log.info("Square setBeanName is called and the beanName is " + beanName);	
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext context)
+			throws BeansException {
+		log.info("Square setApplicationContext is called" + context);
+		this.context = context;
 	}
 }
